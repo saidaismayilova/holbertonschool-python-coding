0e3402d2802498f0e83fa3402dabb6d7d85fef12
saidaismayilova <iseide40@gmail.com>
2025-11-23 19:13:23 +0000

    salam

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """3-square.py"""
 
+
 class Square:
     """Defines a square with private size, getter/setter, and area computation"""
 
     def __init__(self, size=0):
-        """Initialize the square with optional size using the setter for validation"""
+        """Initialize the square with optional size using the setter
+        for validation
+        """
         self.size = size  # uses the setter for validation
 
     @property
